9578f4bab051e58a982ee4c6a140ac02b4e994c7
anxin <anxin@anxin.com>
2015-11-27 12:12:51 +0800

    finished home page's wire frame layout.

--- application/views/header.php
+++ application/views/header.php
@@ -5,13 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title ng-bind="title"><?= $title ?></title>
+    <title ng-bind="title"><?= isset($title) ? $title : '铁杆欢迎您！' ?></title>
 
     <?php
     //设置默认值
     $css_js_version = $this->config->config['css_js_version'];;
     $css = (isset($css) ? $css : array());
     ?>
+
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap-3.3.4/css/bootstrap.min.css"/>
 
     <?php foreach ($css as $cssFile): ?>
         <link rel="stylesheet" type="text/css" href="<?= $cssFile ?>?version=<?= $css_js_version ?>"/>
@@ -20,3 +22,22 @@
 </head>
 
 <body>
+
+<div class="container-fluid">
+    <div class="row header" style="padding-top: 1em;border-bottom: 2px solid gray;">
+        <div class="col-xs-2">
+            <p class="img-thumbnail" style="width:200px; text-align: center;padding: 10px;">tiegan logo</p>
+        </div>
+
+        <div class="col-xs-10" style="text-align: center;padding-top: 10px;">
+            <span class="header-menu">目的地</span>
+            <span class="header-menu">锦囊</span>
+            <span class="header-menu">行程助手</span>
+            <span class="header-menu">社区</span>
+            <span class="header-menu">最世界</span>
+            <span class="header-menu">自由行</span>
+            <span class="header-menu">酒店</span>
+            <span class="header-menu">预订</span>
+        </div>
+    </div>
+</div>
