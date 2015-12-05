2c49bc1938690e9bd2503fb2f1a6bf677b177121
tecshuttle <tecshuttle@gmail.com>
2015-12-05 11:45:19 +0800

    alter tree model field name.

--- js/admin/articles/model/Tree.js
+++ js/admin/articles/model/Tree.js
@@ -2,7 +2,7 @@ Ext.define('Tiegan.model.Tree', {
     extend: 'Ext.data.Model',
     fields: [
         { name: 'id', type: 'string', mapping: 'id' },
-        { name: 'text', type: 'string', mapping: 'name' },
+        { name: 'text', type: 'string', mapping: 'text' },
         { name: 'leaf', type: 'boolean', mapping: 'leaf' },
         { name: 'type', type: 'string'},
         { name: 'value', type: 'string'}
