77238c0fdba16bb84ed4c734d5b5767c5b64537b
tecshuttle <tecshuttle@gmail.com>
2016-01-09 16:15:16 +0800

    内页顶部

--- application/views/mini_header.php
+++ application/views/mini_header.php
@@ -3,7 +3,7 @@
     <!-- nav -->
     <div class="nav">
         <ul class="nav-ul">
-			<li class="nav-list><a class="nav-span" href="/equipments" title="首页">首页</a></li>
+			<li class="nav-list"><a class="nav-span" href="#" title="首页">首页</a></li>
             <li class="nav-list <?= ($menu == 'product' ? 'nav-list-selected' : '') ?>"><a class="nav-span" href="/equipments" title="朝圣">朝圣</a></li>
             <li class="nav-list <?= ($menu == 'back' ? 'nav-list-selected' : '') ?>"><a class="nav-span" href="/cat/225" title="荣归">荣归</a></li>
             <li class="nav-list <?= ($menu == 'travel' ? 'nav-list-selected' : '') ?>"><a class="nav-span" href="/cat/223" title="旅行那些事儿">旅行那些事儿</a></li>
